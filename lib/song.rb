264b7eaccaedc10340e10c5e94720bebf5631b23
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-20 02:42:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,10 @@ class Song
   
     attr_accessor :name, :artist, :genre 
   
-    def initialize(artist_name)
-      @artist = artist_name
+    def initialize(name, artist, genre)
+      @name = name 
+      @artist = artist
+      @genre = genre
     end
     
   

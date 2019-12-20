7b29d2d37d8704e9001375769cacb9a7bc23065d
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-20 03:43:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,16 @@ class Song
   end
   
   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] #if the genre is already in the hash
+        genre_count[genre] += 1
+      else #if it's a new genre
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
 
     #   if artist_count[artist] #if the artist is already in the hash
